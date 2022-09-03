ce72f026265a18d04c478c1cc0de7b852f154dc3
Sabi Viji <sabipalathinkal10@gmail.com>
2022-09-03 11:19:41 +0530

    firebase init added back in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:get/get.dart';
 import 'package:serviz/routes.dart';
 
-import 'screens/create_join_group.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:serviz/screens/homepg.dart';
-import 'firebase_options.dart';
-import 'package:serviz/screens/welcome_screen.dart';
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
